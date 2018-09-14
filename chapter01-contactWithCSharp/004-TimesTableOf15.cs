a01026f608ea48ac0515529b24fdafd165db774c
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-14 10:02:38 +0200

    Exercise 004:Times table of 15

---
+++ chapter01-contactWithCSharp/004-TimesTableOf15.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class TimesTableOf15
+{
+    public static void Main()
+    {
+        Console.WriteLine("15x0=0");
+        Console.WriteLine("15x1=15");
+        Console.WriteLine("15x2=30");
+        Console.WriteLine("15x3=45");
+        Console.WriteLine("15x4=60");
+        Console.WriteLine("15x5=75");
+        Console.WriteLine("15x6=90");
+        Console.WriteLine("15x7=105");
+        Console.WriteLine("15x8=120");
+        Console.WriteLine("15x9=135");
+        Console.WriteLine("15x10=150");
+    }
+}
