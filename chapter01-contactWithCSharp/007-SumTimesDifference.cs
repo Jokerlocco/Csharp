012352c7e6147db44a5429c2b378276cd898ee43
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-15 10:05:27 +0200

    Exercise 007: Sum times difference

---
+++ chapter01-contactWithCSharp/007-SumTimesDifference.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class SumTimesDifference
+{
+    public static void Main()
+    {
+        Console.WriteLine( (3+5) * (3-5) );
+        Console.WriteLine( 3*3 - 5*5 );
+    }
+}
