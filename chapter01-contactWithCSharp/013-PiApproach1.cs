bb080bad492dbf198a9b307554ac1f93648334fd
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-17 00:19:37 +0200

    Exercise 013: Pi as 22/7

---
+++ chapter01-contactWithCSharp/013-PiApproach1.cs
@@ -0,0 +1,13 @@
+// Approximation of Pi, version 1 (22 / 7, as integer)
+// Sergio Ruescas
+
+using System;
+
+public class PiApproach
+{
+    public static void Main()
+    {
+        Console.Write("Pi is approx ");
+        Console.WriteLine(22/7);
+    }
+}
