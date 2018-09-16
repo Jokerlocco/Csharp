1b84952201b50eefa3b1bc30cdc2388002d23c3b
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-17 00:20:02 +0200

    Exercise 014: Pi as 22.0/7.0

---
+++ chapter01-contactWithCSharp/014-PiApproach2.cs
@@ -0,0 +1,13 @@
+// Approximation of Pi, version 2 (22 / 7, as a real number)
+// Sergio Ruescas
+
+using System;
+
+public class PiApproach2
+{
+    public static void Main()
+    {
+        Console.Write("Pi is approx ");
+        Console.WriteLine(22/7.0);
+    }
+}
