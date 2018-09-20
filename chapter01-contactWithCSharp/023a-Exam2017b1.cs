6e80d7bb82d609a2d6ebd146d0ab6d2ef0553dc1
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-20 11:07:01 +0200

    Exercise 023: Exam of chapter 1 (2017), part B, version 1

---
+++ chapter01-contactWithCSharp/023a-Exam2017b1.cs
@@ -0,0 +1,32 @@
+/* Manuel Rojas Henarejos
+19/09/2018 */
+
+/*
+Usage example:
+
+Introduce el peso en libras: 5
+5 libras son 80 onzas
+5 libras son 2265 gramos
+
+(Version 1: only integer values)
+
+*/
+
+
+using System;
+public class ExamenB
+{
+    public static void Main()
+   {
+        int libras, onzas, gramos;
+        Console.Write("Introduce el peso en libras: ");
+        libras=Convert.ToInt32(Console.ReadLine());
+
+        onzas= libras * 16;
+        gramos= libras * 453;
+
+        Console.WriteLine("{0} libras son {1} onzas", libras, onzas);
+        Console.Write("{0} libras son {1} gramos", libras, gramos);
+    }
+}
+
