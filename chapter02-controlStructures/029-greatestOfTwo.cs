33f4840d3751332a3425015cf69ce7c41ad471d0
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-22 22:24:40 +0200

    Exercise 029: Greatest of two

---
+++ chapter02-controlStructures/029-greatestOfTwo.cs
@@ -0,0 +1,25 @@
+// Ivan Lazcano Sindin
+// This program asks for 2 numbers 
+//   and says which one is the greatest
+
+using System;
+
+public class Greatest
+{
+    public static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter another number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        
+        if ( num1 > num2 )
+            Console.WriteLine("The first one is the greatest");
+        else if( num2 == num1 )
+            Console.WriteLine("They are the same number");
+        else
+            Console.WriteLine("The second one is the greatest");
+    }
+    
+}
