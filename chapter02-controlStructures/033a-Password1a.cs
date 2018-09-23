9b8c2942f8fc189b5f2620b18cbb85e146edaec1
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-23 15:53:09 +0200

    Exercise 033: Password (v1: one attempt)

---
+++ chapter02-controlStructures/033a-Password1a.cs
@@ -0,0 +1,19 @@
+// Pide contraseña al usuario si es correcta 
+//    "1234" dira contraseña correcta sino incorrecta
+// Miguel Isidro Blasco
+
+using System;
+
+public class Password1
+{
+    public static void Main()
+    {
+        Console.Write("Password: ");
+        int password = Convert.ToInt32(Console.ReadLine());
+        
+        if(password == 1234)
+            Console.Write("Access granted!");
+        else
+            Console.Write("Access denied!");
+    }
+}
