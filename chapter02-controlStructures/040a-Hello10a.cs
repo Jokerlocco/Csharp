baf90504b05b04cfc5ca7411dae27a1ce9f9484e
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-27 10:14:25 +0200

    Exercise 040: Display Hello 10 times

---
+++ chapter02-controlStructures/040a-Hello10a.cs
@@ -0,0 +1,17 @@
+// Display "Hello" 10 times
+
+using System;
+
+public class Hello10a
+{
+    public static void Main()
+    {
+        int count = 0;
+        
+        while ( count < 10 )
+        {
+            count = count + 1;
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ chapter02-controlStructures/040b-Hello10b.cs
@@ -0,0 +1,17 @@
+// Display "Hello" 10 times
+
+using System;
+
+public class Hello10b
+{
+    public static void Main()
+    {
+        int count = 1;
+        
+        while ( count <= 10 )
+        {
+            Console.WriteLine("Hello");
+            count = count + 1;
+        }
+    }
+}
