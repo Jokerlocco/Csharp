16ec0cc3a6bc24d09f6d2951985b6a3d9c314e9a
Nacho Cabanes <nacho@nachocabanes.com>
2018-09-28 09:53:30 +0200

    Exercise 048: Count 10 to 20, for

---
+++ chapter02-controlStructures/048-TenToTwenty.cs
@@ -0,0 +1,12 @@
+// Display 10 to 20 (for)
+
+using System;
+
+public class TenToTwenty
+{
+    public static void Main()
+    {
+        for (int i = 10  ; i <= 20 ; i++)
+            Console.WriteLine(i);
+    }
+}
