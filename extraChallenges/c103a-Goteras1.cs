7a96b21a86d51c4844fdcf3d13104960f4660625
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-04 09:21:58 +0200

    Challenge 1.03: Goteras (C#, corrected)

--- extraChallenges/c103a-Goteras1.cs
+++ extraChallenges/c103a-Goteras1.cs
@@ -32,23 +32,21 @@ public class Gotera
             minutes = (input % 3600) / 60;
             seconds = input % 60;
         
-            for ( int j = input ; j == input ; j++)
-            {
-                if ( hours < 10 )
-                    Console.Write("0{0}:",hours);
-                else
-                    Console.Write("{0}:",hours);
-            
-                if ( minutes < 10 )
-                    Console.Write("0{0}:",minutes);
-                else
-                    Console.Write("{0}:",minutes);
-            
-                if ( seconds < 10 )
-                    Console.Write("0{0}",seconds);
-                else
-                    Console.Write("{0}",seconds);
-            }
+            if ( hours < 10 )
+                Console.Write("0{0}:",hours);
+            else
+                Console.Write("{0}:",hours);
+        
+            if ( minutes < 10 )
+                Console.Write("0{0}:",minutes);
+            else
+                Console.Write("{0}:",minutes);
+        
+            if ( seconds < 10 )
+                Console.Write("0{0}",seconds);
+            else
+                Console.Write("{0}",seconds);
+
             Console.WriteLine();
         }
     }
