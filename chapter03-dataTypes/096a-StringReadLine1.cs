2363ffa3ea3adaff4494ea464d3489caf7db7b06
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-16 09:44:22 +0200

    Exercise 096: Contact with "string" + Readline

---
+++ chapter03-dataTypes/096a-StringReadLine1.cs
@@ -0,0 +1,15 @@
+// Ruth martínez
+// Contact with strings
+
+using System;
+
+public class Strings1a
+{
+    public static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("Hi, {0}", name);
+    }    
+}
---
+++ chapter03-dataTypes/096b-StringReadLine2.cs
@@ -0,0 +1,15 @@
+// Ruth martínez
+// Contact with strings (2)
+
+using System;
+
+public class Strings1b
+{
+    public static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("Hi, " + name);
+    }    
+}
