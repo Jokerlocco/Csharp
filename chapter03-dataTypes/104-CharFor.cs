8759bdf4deea5763922a37d6ad55f479bb211d05
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-18 09:23:55 +0200

    Exercise 104: "char" + "for"

---
+++ chapter03-dataTypes/104-CharFor.cs
@@ -0,0 +1,14 @@
+// Char 3 (Abecefor)
+
+using System;
+
+class Char3
+{
+    static void Main()
+    {
+        for ( char letter = 'A' ; letter <= 'Z' ; letter++ )
+        {
+            Console.Write("{0} ", letter);
+        }
+    }
+}
