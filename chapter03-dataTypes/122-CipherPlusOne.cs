a87ce5898556ba7dc766ba90c5777066e4c2c69d
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-23 14:57:29 +0200

    Exercise 122: Cipher, plus one

---
+++ chapter03-dataTypes/122-CipherPlusOne.cs
@@ -0,0 +1,25 @@
+// Adrián Navarro Gabino
+
+using System;
+
+public class Cifrado
+{
+    public static void Main()
+    {
+        
+        Console.Write("Enter some text: ");
+        string text = Console.ReadLine();
+        string encryptedText = "";
+        string decryptedText = "";
+        
+        // Encrypt
+        foreach (char c in text)
+            encryptedText += Convert.ToChar(c + 1);
+        Console.WriteLine("Encrypted: " + encryptedText);
+
+        // Decrypt
+        foreach (char c in encryptedText)
+            decryptedText += Convert.ToChar(c - 1);
+        Console.WriteLine("Decrypted: " + decryptedText);
+    }
+}
