a75492f19fc1283cafe99031370b5e0e3c93fcf2
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-25 14:47:26 +0200

    Exercise 128: Sphere + var

---
+++ chapter03-dataTypes/128-SphereVar.cs
@@ -0,0 +1,20 @@
+﻿// Area and volume of a sphere (var)
+
+using System;
+
+class Sphere2
+{
+    static void Main()
+    {
+        const double PI = 3.1415926535;
+
+        Console.Write("Enter radius: ");
+        var radius = Convert.ToSingle(Console.ReadLine());
+
+        var area = 4 * PI * radius * radius;
+        Console.WriteLine("Area = " + area);
+
+        var volume = 4.0f / 3.0f * PI * radius * radius * radius;
+        Console.WriteLine("Volume = " + volume);
+    }
+}
