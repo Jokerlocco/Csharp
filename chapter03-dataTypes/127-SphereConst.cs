89b8d18bc5af292a2440979420cb0466e8db1a70
Nacho Cabanes <nacho@nachocabanes.com>
2018-10-25 14:44:59 +0200

    Exercise 127: Sphere, const

---
+++ chapter03-dataTypes/127-SphereConst.cs
@@ -0,0 +1,20 @@
+﻿// Area and volume of a sphere (float + const)
+
+using System;
+
+class Sphere2
+{
+    static void Main()
+    {
+        const float PI = 3.1415926535f;
+
+        Console.Write("Enter radius: ");
+        float radius = Convert.ToSingle(Console.ReadLine());
+
+        float area = 4 * PI * radius * radius;
+        Console.WriteLine("Area = " + area);
+        
+        float volume = 4.0f / 3.0f * PI * radius * radius * radius;
+        Console.WriteLine("Volume = " + volume);
+    }
+}
