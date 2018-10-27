3ab5e9b26b58e95cd91c2f3d32b85a5418b196be
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-10-27 17:42:14 +0200

    Exercise 136: "if" + switch (exam of 2017)

---
+++ chapter03-dataTypes/136-IfSwitch2017.cs
@@ -0,0 +1,53 @@
+﻿//Gonzalo Arques
+using System;
+public class NombrePrograma
+{
+    public static void Main()
+    {
+
+        Console.Write("Inserta un símbolo: ");
+        string dato = (Console.ReadLine());
+
+        switch (dato)
+        {
+            case "<":
+            case ">":
+            case "<=":
+            case ">=":
+            case "==":
+            case "!=": 
+                Console.WriteLine("Es un comparador"); 
+                break;
+
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "%": 
+                Console.WriteLine("Es un operador aritmético"); 
+                break;
+
+            case "\"":
+            case "'":
+            case "(":
+            case ")": 
+                Console.WriteLine("Es un delimitador de texto"); 
+                break;
+
+            default: 
+                Console.WriteLine("Es otra cosa"); 
+                break;
+        }
+
+        if ((dato == "<") || (dato == ">") || (dato == "<=")
+                || (dato == ">=") ||  (dato == "==") || (dato == "!="))
+            Console.WriteLine("Es un comparador");
+        else if ((dato == "+") || (dato == "-") || (dato == "*")
+                || (dato == "/") ||  (dato == "%"))
+            Console.WriteLine("Es un operador aritmético");
+        else if ((dato == "\"") || (dato == "'"))
+            Console.WriteLine("Es un delimitador de texto");
+        else
+            Console.WriteLine("Es otro tipo de dato");
+    }
+}
