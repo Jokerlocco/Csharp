d5e4440f8467456b329fb76a4ae12fd9fe4a8df6
Nacho Cabanes <nacho@nachocabanes.com>
2018-11-01 10:53:06 +0100

    Exercise 145: Split a sentence into words

---
+++ chapter04-arraysStruct/145-SplitText.cs
@@ -0,0 +1,15 @@
+﻿// Gonzalo Arques
+// Split a sentence into words
+
+using System;
+class SplitASentence
+{
+    static void Main()
+    {
+        Console.Write("Enter some text: ");
+        string[] words = Console.ReadLine().Split();
+
+        for (int i = 0; i < words.Length; i++)
+            Console.WriteLine(words[i]);
+    }
+}
