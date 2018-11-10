49efab4384f3887ebc83bb56e3d2e8b51e0ffb43
Nacho Cabanes <nacho@nachocabanes.com>
2018-11-10 10:36:28 +0100

    Exercise 167: String to uppercase

--- chapter04-arraysStruct/167-Uppercase.cs
+++ chapter04-arraysStruct/167-Uppercase.cs
@@ -7,6 +7,11 @@ public class Uppercase
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
         
+        // Note: the next line does not work,
+        // we must save the changes
+        // name.ToUpper();
+        
+        // This is the correct way
         name = name.ToUpper();
         
         Console.Write("Now your name is: ");
