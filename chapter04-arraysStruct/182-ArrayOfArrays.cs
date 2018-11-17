4419ed480a24989df2c95894be0f84ae29b4f8cb
Nacho Cabanes <nacho@nachocabanes.com>
2018-11-17 10:31:37 +0100

    Exercise 182: Array of arrays

---
+++ chapter04-arraysStruct/182-ArrayOfArrays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class ArrayOfArrays
+{
+    public static void Main()
+    {
+        int[][] myArray = new int[3][];
+
+        myArray[0] = new int[6];
+        myArray[1] = new int[5];
+        myArray[2] = new int[4];
+
+        for (int row = 0; row < myArray.Length; row++)
+        {
+            for (int column = 0; column < myArray[row].Length; column++)
+            {
+                Console.WriteLine("Enter data for row {0} "
+                    + "and col {1}", row+1, column+1);
+                myArray[row][column] = Convert.ToInt32(
+                    Console.ReadLine());
+            }
+        }
+
+
+        for (int row = 0; row < myArray.Length; row++)
+        {
+            for (int column = 0; column < myArray[row].Length; column++)
+            {
+                Console.Write("[{0},{1}]=", row+1, column+1);
+                Console.WriteLine(myArray[row][column]);
+            }
+        }
+    }
+}
