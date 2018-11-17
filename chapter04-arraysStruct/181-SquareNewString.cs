ce424992b3f0ec65afad86d6e264e5aab1a912a3
Nacho Cabanes <nacho@nachocabanes.com>
2018-11-17 10:31:00 +0100

    Exercise 181: Square, new string

---
+++ chapter04-arraysStruct/181-SquareNewString.cs
@@ -0,0 +1,18 @@
+﻿// Javier Benajes
+
+using System;
+
+public class SquareNewString
+{
+    public static void Main()
+    {
+        Console.Write("Size of the square? ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        string line = new string('*', size);
+        
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
