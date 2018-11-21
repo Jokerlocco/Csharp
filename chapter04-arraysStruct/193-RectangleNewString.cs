d16a9d909dcf27aa9e2c4a296300c3ccdfeb3e3d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-11-21 19:55:34 +0100

    Exercise 193: Hollow Rectangle, new string

---
+++ chapter04-arraysStruct/193-RectangleNewString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class RectangleNewString
+{
+    public static void Main()
+    {
+        Console.Write("Width of the rectangle? ");
+        int width = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Height of the rectangle? ");
+        int height = Convert.ToInt32(Console.ReadLine());
+        
+        string line = new string('#', width);
+        string spaces = new string(' ', width-2);
+        
+        Console.WriteLine(line);
+        for (int i = 0; i < height-2; i++)
+        {
+            Console.WriteLine("#" + spaces + "#");
+        }
+        Console.WriteLine(line);
+    }
+}
