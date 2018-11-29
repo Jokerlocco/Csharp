ea422550d8d9262f8ce8cb9dd80a53322796f89e
Nacho Cabanes <nacho@nachocabanes.com>
2018-11-29 22:34:21 +0100

    Exercise 208: Contact with functions

---
+++ chapter05-functions/208-FunctionSayHelloToNacho.cs
@@ -0,0 +1,16 @@
+// Adrián Navarro Gabino
+
+using System;
+
+public class Greeting
+{
+    public static void SayHelloToNacho()
+    {
+        Console.WriteLine("Hi, Nacho");
+    }
+    
+    public static void Main()
+    {
+        SayHelloToNacho();
+    }
+}
