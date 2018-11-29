a792478c2caa3a4ba862a37a994b5cbc1f4a6ee4
Nacho Cabanes <nacho@nachocabanes.com>
2018-11-29 22:34:57 +0100

    Exercise 209: Functions with a parameter

---
+++ chapter05-functions/209-FunctionSayHello.cs
@@ -0,0 +1,17 @@
+﻿// Javier Benajes
+
+using System;
+
+public class Greeting2
+{
+    public static void SayHello(string name)
+    {
+        Console.WriteLine("Hi, {0}", name);
+    }
+    
+    public static void Main()
+    {
+        SayHello("Nacho");
+        SayHello("Javi");
+    }
+}
