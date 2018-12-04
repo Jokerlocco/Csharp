ce512756cac255ff778e69ae1fb833ab85bae1e5
Nacho Cabanes <nacho@nachocabanes.com>
2018-12-04 10:01:20 +0100

    Exercise 227: Function MaxArray

---
+++ chapter05-functions/227-MaxArray.cs
@@ -0,0 +1,25 @@
+﻿// Diego Aníbal Lezcano Reissner
+
+using System;
+
+class MaxArray
+{
+    static int Max(int[] numbers)
+    {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if(numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 20, 30, -5, 2 };
+        Console.WriteLine(Max(numbers));
+    }
+}
