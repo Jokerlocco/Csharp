351592c2b0b9462fe8d5fd9ba1398b45e87a21d9
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-12-05 20:19:11 +0100

    Exercise 232: Function Invert (2 versions)

---
+++ chapter05-functions/232a-FunctionInvert1.cs
@@ -0,0 +1,22 @@
+﻿// Javier Benajes
+
+using System;
+
+public class FunctionInvert1
+{
+    public static string Invert(string s)
+    {
+        string inverted = "";
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            inverted += s[i];
+        }
+        return inverted;
+    }
+
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        Console.Write(Invert(text));
+    }
+}
---
+++ chapter05-functions/232b-FunctionInvert2.cs
@@ -0,0 +1,22 @@
+﻿// Inverting a string... with foreach
+
+using System;
+
+public class FunctionInvert2
+{
+    public static string Invert(string s)
+    {
+        string inverted = "";
+        foreach (char c in s)
+        {
+            inverted = c + inverted;
+        }
+        return inverted;
+    }
+
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        Console.Write(Invert(text));
+    }
+}
