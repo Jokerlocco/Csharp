74ed6ce4fdf48085151d95cc41f4047569ad84c8
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-12-05 20:39:14 +0100

    Exercise 234: Function Factorial, iterative

---
+++ chapter05-functions/234-FactorialIterative.cs
@@ -0,0 +1,22 @@
+﻿// José Vicente Antón Castelló
+
+using System;
+
+class IterativeFactorial
+{
+    static int Factorial(int num)
+    {
+        int total = 1;
+        
+        for (int i = 1; i <= num; i++)
+        {
+            total *= i;
+        }
+        return total;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Factorial(5));
+    }
+}
