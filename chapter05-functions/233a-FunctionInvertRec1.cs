d35d1dd8e80226a745a252535eb5ab6f43bfafdf
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-12-05 20:19:48 +0100

    Exercise 233: Function Invert recursive (2 versions)

---
+++ chapter05-functions/233a-FunctionInvertRec1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class FunctionInvertRecursive
+{
+    public static string Invert(string s)
+    {
+        if (s.Length <= 1)
+        {
+            return s;
+        }
+        else
+        {
+            return s[s.Length - 1] 
+                + Invert(s.Substring(0, s.Length - 1));
+        }
+    }
+
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        Console.Write(Invert(text));
+    }
+}
---
+++ chapter05-functions/233b-FunctionInvertRec2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class FunctionInvertRecursive2
+{
+    public static string Invert(string s)
+    {
+        if (s.Length <= 1)
+        {
+            return s;
+        }
+        else
+        {
+            return Invert(s.Substring(1)) + s[0];
+        }
+    }
+
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        Console.Write(Invert(text));
+    }
+}
