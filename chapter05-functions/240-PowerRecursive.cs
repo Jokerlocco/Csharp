260a8fdce83bea932c939ddb7e6ba19dae92feee
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-12-07 16:56:18 +0100

    Exercise 240: Function Power (recursive)

---
+++ chapter05-functions/240-PowerRecursive.cs
@@ -0,0 +1,21 @@
+﻿// José Vicente Antón Castelló
+
+using System;
+
+class RecursivePower
+{
+    static ulong Power(ulong b, ulong e)
+    {
+        if (e == 0)
+        {
+            return 1;
+        }
+        
+        return b * Power(b , e - 1);
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine(Power(2, 3));
+    }
+}
