a0f647ee476869c679bec48e5c5c8ff4389ba52d
Nacho Cabanes <nacho@nachocabanes.com>
2018-12-18 00:18:59 +0100

    Exercise 249: Count even & odd digits (2 versions)

---
+++ chapter05-functions/249a-CountEvenOdd1.cs
@@ -0,0 +1,34 @@
+﻿// Diego Aníbal Lezcano Reissner
+
+using System;
+using System.Text;
+
+
+class EvenOdds
+{
+    static void CountEvenOdd(int n, ref int odds, ref int evens)
+    {
+        string nStr = Convert.ToString(n);
+        for(int i = 0;i < nStr.Length;i++)
+        {
+            if (nStr[i] % 2 == 0)
+            {
+                evens++;
+            }
+            else
+            {
+                odds++;
+            }
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        int evens = 0, odds = 0;
+
+        CountEvenOdd(123456777, ref odds, ref evens);
+        Console.WriteLine("Even digits = " + evens);
+        Console.WriteLine("Odd digits = " + odds);
+    }
+}
+
---
+++ chapter05-functions/249b-CountEvenOdd2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+class EvenOdds2
+{
+    static void CountEvenOdd(int n, ref int odds, ref int evens)
+    {
+        while (n > 0)
+        {
+            int lastDigit = n% 10;
+
+            if (lastDigit % 2 == 0)
+                evens++;
+            else
+                odds++;
+            
+            n /= 10;            
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        int evens = 0, odds = 0;
+
+        CountEvenOdd(123456777, ref odds, ref evens);
+        Console.WriteLine("Even digits = " + evens);
+        Console.WriteLine("Odd digits = " + odds);
+    }
+}
+
