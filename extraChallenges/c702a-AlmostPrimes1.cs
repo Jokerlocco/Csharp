cf63f69b19cdf519dba7a22129a7b6a0b1b2f7c5
Nacho Cabanes <nacho@nachocabanes.com>
2018-12-19 13:19:11 +0100

    Challenge 7.02: Almost prime (first version)

---
+++ extraChallenges/c702a-AlmostPrimes1.cs
@@ -0,0 +1,73 @@
+// Adrián Navarro Gabino
+
+// Almost prime = two (not necessarily distinct) prime factors.
+// How many almost prime numbers there are in certain integer intervals?
+
+/*
+Sample input
+
+6
+1 10
+10 20
+30000 40000
+400000 500000
+6000000 7000000
+1 9999999
+
+Sample output
+
+4
+3
+2329
+1904324
+*/
+
+using System;
+
+public class AlmostPrimes
+{
+    public static bool IsAlmostPrime(int number)
+    {
+        for(int i = 2; i <= Math.Sqrt(number); i++)
+            if(number % i == 0 && IsPrime(i))
+                if(IsPrime(number / i))
+                    return true;
+        
+        return false;
+    }
+    
+    public static bool IsPrime(int number)
+    {
+        for(int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if(number % i == 0)
+                return false;
+        }
+        
+        return true;
+    }
+    
+    
+    public static void Main()
+    {
+        int cases = Convert.ToInt32(Console.ReadLine());
+        
+        for(int currentCase = 0; currentCase < cases; currentCase++)
+        {
+            string[] aux = Console.ReadLine().Split();
+            
+            int lowerLimit = Convert.ToInt32(aux[0]);
+            int upperLimit = Convert.ToInt32(aux[1]);
+            
+            int count = 0;
+            
+            for(int i = lowerLimit; i <= upperLimit; i++)
+            {
+                if(IsAlmostPrime(i))
+                    count++;
+            }
+            
+            Console.WriteLine(count);
+        }
+    }
+}
