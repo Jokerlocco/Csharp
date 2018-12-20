bd45df7096bf637361ef84b99cffcc3a4f38ce23
Nacho Cabanes <nacho@nachocabanes.com>
2018-12-20 10:10:14 +0100

    Challenge 5.05: Cameos of Stan Lee

---
+++ extraChallenges/c505a-CameosOfStanLee.cs
@@ -0,0 +1,77 @@
+// https://www.aceptaelreto.com/problem/statement.php?id=475
+// Cameos de Stan Lee
+// Cantidad de veces que aparece "stan lee" en cada frase
+// Nacho, 19-Dic-2018
+
+/*
+Entrada de ejemplo
+
+12
+Que me diga donde estan, le exijo!
+Eres tan lento que te ganaria una oruga. Calla y salta, no le temas.
+Esto es canela, verdad que si?
+Stan Stan Lee Lee
+Stan Loo
+St anlEe
+St an lEe
+stanle
+stanley
+stanlye
+tsanlee
+stanleestanlee
+
+Salida de ejemplo
+ 
+1
+2
+1
+1
+0
+1
+1
+0
+0
+0
+0
+0
+2
+
+*/
+
+using System;
+
+class YourFriendlyNeighbour
+{
+    static void Main()
+    {
+
+        int casos;
+        string frase;
+        string busqueda = "stanlee";
+        int cantidadLetras = busqueda.Length;
+
+        casos = Convert.ToInt32(Console.ReadLine());
+
+        for (int c = 0; c < casos; c++)
+        {
+            int apariciones = 0;
+            int letraActual = 0;
+
+            frase = Console.ReadLine();
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (Char.ToLower(frase[i]) == busqueda[letraActual])
+                {
+                    letraActual++;
+                    if (letraActual == cantidadLetras)
+                    {
+                        apariciones++;
+                        letraActual = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine(apariciones);
+        }
+    }
+}
