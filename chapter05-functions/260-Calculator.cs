2429285f8e772a39e7a364cafd10130cdd959a12
Nacho Cabanes <nacho@nachocabanes.com>
2018-12-26 16:14:24 +0100

    Exercise 26: Calculator

---
+++ chapter05-functions/260-Calculator.cs
@@ -0,0 +1,41 @@
+﻿// Javier Benajes
+
+using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 3)
+        {
+            int firstNumber = Convert.ToInt32(args[0]);
+            int secondNumber = Convert.ToInt32(args[2]);
+            string operation = args[1];
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(firstNumber + secondNumber);
+                    break;
+                case "-":
+                    Console.WriteLine(firstNumber - secondNumber);
+                    break;
+                case "*":
+                case "x":
+                case "X":
+                    Console.WriteLine(firstNumber * secondNumber);
+                    break;
+                case "/":
+                    Console.WriteLine(firstNumber / secondNumber);
+                    break;
+                default:
+                    Console.WriteLine("Operator not valid.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not enough parameters.");
+        }
+    }
+}
