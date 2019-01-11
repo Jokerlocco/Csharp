cbc721dc84a37571bd502e121090d546dc2d6b16
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-01-11 17:10:46 +0100

    Exercise 283: Class Car

---
+++ chapter06-classes/283-ClassCar.cs
@@ -0,0 +1,37 @@
+﻿// Class Car
+
+using System;
+
+class Car
+{
+    private string brand;
+    private string model;
+    private double power;
+    private byte doors;
+
+    public void Start()
+    {
+        // TO DO
+    }
+
+    public void Accelerate()
+    {
+        // TO DO
+    }
+
+    public void Brake()
+    {
+        // TO DO
+    }
+}
+
+// ----------------------------------
+
+class CarTest
+{
+    static void Main(string[] args)
+    {
+        Car c = new Car();
+        c.Start();
+    }
+}
