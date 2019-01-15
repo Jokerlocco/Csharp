96e596fad57031ff77c368b5faee227d40e9fe3d
Nacho Cabanes <nacho@nachocabanes.com>
2019-01-15 19:42:07 +0100

    Exercise 293: Constructor & Destructor 2

---
+++ chapter06-classes/293-ConstructorDestructor2.cs
@@ -0,0 +1,44 @@
+﻿// Constructors & Destructors 2: Calling order
+// Javier Benajes
+
+using System;
+
+class ConstructorDestructor
+{
+    public ConstructorDestructor()
+    {
+        Console.WriteLine("Constructor");
+    }
+
+    ~ConstructorDestructor()
+    {
+        Console.WriteLine("Destructor");
+    }
+
+    public void Pausar()
+    {
+        Console.Write("Press Enter...");
+        Console.ReadLine();
+    }
+}
+
+class ConstructorDestructor2 : ConstructorDestructor
+{
+    public ConstructorDestructor2()
+    {
+        Console.WriteLine("Constructor2");
+    }
+
+    ~ConstructorDestructor2()
+    {
+        Console.WriteLine("Destructor2");
+    }
+}
+
+class ConstructorDestructorTest
+{
+    public static void Main()
+    {
+        ConstructorDestructor2 cd2 = new ConstructorDestructor2();
+    }
+}
