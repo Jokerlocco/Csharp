71e5e947ae3c709fb8b2a1b952fdeb0c913cde20
Nacho Cabanes <nacho@nachocabanes.com>
2019-01-15 19:38:57 +0100

    Exercise 292: Constructor & Destructor

---
+++ chapter06-classes/292-ConstructorDestructor.cs
@@ -0,0 +1,32 @@
+﻿// Constructor & Destructor
+// Javier Benajes
+
+using System;
+
+class ConstructorDestructor
+{
+    public ConstructorDestructor()
+    {
+        Console.WriteLine("Constructor");
+    }
+
+    ~ConstructorDestructor()
+    {
+        Console.WriteLine("Destructor");
+    }
+
+    public void Pause()
+    {
+        Console.WriteLine("Press Enter...");
+        Console.ReadLine();
+    }
+}
+
+class ConstructorDestructorTest
+{
+    public static void Main()
+    {
+        ConstructorDestructor cd = new ConstructorDestructor();
+        cd.Pause();
+    }
+}
