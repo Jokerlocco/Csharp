95296ebaf548e693934621b93149b9fc4646512d
Nacho Cabanes <nacho@nachocabanes.com>
2019-01-18 11:47:27 +0100

    Exercise 296: Class Table + inheritance

---
+++ chapter06-classes/296-TableInheritance.cs
@@ -0,0 +1,80 @@
+// Table + Leg + LittleTable + TableTest
+
+// -------------------------------------------------------------
+
+class Table
+{
+    protected int width, length;
+    protected string color;
+    protected Leg myLeg;
+    
+    public Table(int w, int l, string col)
+    {
+        width = w;
+        length = l;
+        color = col;
+    }
+    
+    public Table()
+    {
+    }
+    
+    public int GetWidth() { return width; }
+    public int GetLength() { return length; }
+    public string GetColor() { return color; }
+    public Leg GetLeg() { return myLeg; }
+    
+    public void SetWidth(int w) { width = w; }
+    public void SetLength(int l) { length = l; }
+    public void SetColor(string c) { color = c; }
+    public void SetLeg(Leg l) { myLeg = l; }
+}
+
+
+// -------------------------------------------------------------
+
+class Leg
+{
+    protected int height;
+    protected string color;
+    
+    public Leg(int h, string col)
+    {
+        height = h;
+        color = col;
+    }
+
+    public int GetHeight() { return height; }
+    public string GetColor() { return color; }
+    
+    public void SetHeight(int h) { height = h; }
+    public void SetColor(string c) { color = c; }
+}
+
+// -------------------------------------------------------------
+
+class LittleTable : Table
+{
+    public LittleTable()
+    {
+        width = 40;
+        length = 60;
+    }
+}
+
+// -------------------------------------------------------------
+
+
+
+class TableTest
+{
+    static void Main(string[] args)
+    {
+        Table t = new Table(80, 120, "oak");
+        Leg l = new Leg(78, "silver");
+        t.SetLeg(l);
+        
+        LittleTable lt = new LittleTable();
+        lt.SetColor("dark brown");
+    }
+}
