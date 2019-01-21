7d24a571501659897b184c3aafe46ebc1ce57a5a
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-01-21 20:46:22 +0100

    Exercise 300: PhotoAlbum + constructors

---
+++ chapter06-classes/300-PhotoAlbumConstructors.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class PhotoAlbum
+{
+    protected int amountOfPages;
+    
+    public PhotoAlbum()
+    {
+        amountOfPages = 16;
+        Console.WriteLine("Album, "+amountOfPages+" pages");
+    }
+    
+    public PhotoAlbum(int pages)
+    {
+        amountOfPages = pages;
+        Console.WriteLine("Album, "+amountOfPages+" pages");
+    }
+
+    public int GetAmountOfPages()
+    {
+        return amountOfPages;
+    }
+}
+
+// ------------------------------
+
+class BigPhotoAlbum : PhotoAlbum
+{
+    public BigPhotoAlbum()
+    {
+        amountOfPages = 64;
+        Console.WriteLine("BigAlbum, "+amountOfPages+" pages");
+    }
+}
+
+// ------------------------------
+
+public class PhotoAlbumTest
+{
+    public static void Main()
+    {
+        // PhotoAlbum album1 = new PhotoAlbum();
+        // PhotoAlbum album2 = new PhotoAlbum(24);
+        BigPhotoAlbum album3 = new BigPhotoAlbum();
+        
+        // Console.WriteLine( "Album 1: "+album1.GetAmountOfPages() );
+        // Console.WriteLine( "Album 2: "+album2.GetAmountOfPages() );
+        // Console.WriteLine( "Album 3: "+album3.GetAmountOfPages() );
+    }
+}
