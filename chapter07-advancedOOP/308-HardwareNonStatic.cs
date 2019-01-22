4785ecaec76bc2c17a3fd6dd30e5098b3b0141df
Nacho Cabanes <nacho@nachocabanes.com>
2019-01-22 09:30:59 +0100

    Exercise 308: Class Hardware + non-static methods

---
+++ chapter07-advancedOOP/308-HardwareNonStatic.cs
@@ -0,0 +1,22 @@
+﻿// Javier Benajes
+
+using System;
+
+public class Hardware
+{
+    public void BorrarPantalla()
+    {
+        for (int i = 0; i < 25; i++)
+            Console.WriteLine();
+    }
+}
+
+class PruebaBorrarPantalla
+{
+    public static void Main()
+    {
+        Hardware hw = new Hardware();
+
+        hw.BorrarPantalla();
+    }
+}
