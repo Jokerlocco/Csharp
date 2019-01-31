648e87c5e1dd2e45bd10ad99da20833f6a83e223
Nacho Cabanes <nacho@nachocabanes.com>
2019-01-31 22:22:49 +0100

    Exercise 343: Encrypter, non-static methods

---
+++ chapter07-advancedOOP/343-ClassEncrypterNonStatic.cs
@@ -0,0 +1,42 @@
+﻿// Javier Benajes
+
+using System;
+
+class Encrypter
+{
+    public string Encrypt(string s)
+    {
+        string newS = "";
+
+        foreach (char c in s)
+        {
+            newS += (char)(c + 1);
+        }
+
+        return newS;
+    }
+
+    public string Decrypt(string s)
+    {
+        string newS = "";
+
+        foreach (char c in s)
+        {
+            newS += (char)(c - 1);
+        }
+
+        return newS;
+    }
+}
+
+class EncrypterTest
+{
+    public static void Main()
+    {
+        Encrypter e = new Encrypter();
+        string nuevoTexto = e.Encrypt("Hola");
+        Console.WriteLine(nuevoTexto);
+        nuevoTexto = e.Decrypt(nuevoTexto);
+        Console.WriteLine(nuevoTexto);
+    }
+}
