3d4c0a347154d1faa55a9e6763d53ee546c6ba16
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-02-14 20:48:46 +0100

    Exercise 357: Queue + ToArray

---
+++ chapter08-dynamicMemory/357-QueueToArray.cs
@@ -0,0 +1,33 @@
+﻿// Queue + ToArray
+// Sergio Ruescas
+
+using System.Collections;
+using System;
+
+class QueueToArray
+{
+    public static void Main()
+    {
+        string input;
+        Queue queue = new Queue();
+        do
+        {
+            input = Console.ReadLine();
+            if (input != "" && input != "quit")
+            {
+                queue.Enqueue(input);
+            }
+            else
+            {
+                Object[] queueCopy = queue.ToArray();
+                for (int i = 0; i < queueCopy.Length; i++)
+                {
+                    Console.Write(queueCopy[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }while (input != "quit") ;
+        
+    }
+}
+
