67114f3da3abfe8766fac26157d6c7b5ebe88e0e
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-02-18 19:55:26 +0100

    Exercise 363: Queue + ToArray + generics

---
+++ chapter08-dynamicMemory/363-QueueToArrayGenerics.cs
@@ -0,0 +1,32 @@
+﻿// Queue + ToArray + Generics
+
+using System.Collections.Generic;
+using System;
+
+class QueueToArray
+{
+    public static void Main()
+    {
+        string input;
+        Queue<string> queue = new Queue<string>();
+        do
+        {
+            input = Console.ReadLine();
+            if (input != "" && input != "quit")
+            {
+                queue.Enqueue(input);
+            }
+            else
+            {
+                string[] queueCopy = queue.ToArray();
+                for (int i = 0; i < queueCopy.Length; i++)
+                {
+                    Console.Write(queueCopy[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }while (input != "quit") ;
+        
+    }
+}
+
