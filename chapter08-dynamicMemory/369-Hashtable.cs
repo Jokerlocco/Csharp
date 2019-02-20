1e27176d7ba563228a2da523b7cf56ed156f2e96
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-02-20 20:28:59 +0100

    Exercise 369: Dictionary - Hashtable

---
+++ chapter08-dynamicMemory/369-Hashtable.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+
+class DictionaryHastable
+{
+
+    public static void Main()
+    {
+        Hashtable diccionario = new Hashtable();
+        diccionario.Add("hello", "hola");
+        diccionario.Add("table", "mesa");
+        diccionario.Add("window", "ventana");
+        diccionario.Add("dog", "perro");
+        diccionario.Add("cat", "gato");
+        diccionario.Add("horse", "caballo");
+        diccionario.Add("sister", "hermana");
+        diccionario.Add("brother", "hermano");
+        diccionario.Add("father", "padre");
+        diccionario.Add("mother", "madre");
+
+        string palabra;
+        do
+        {
+            Console.WriteLine("Introduce una palabra");
+            palabra = Console.ReadLine();
+
+            if (palabra != "")
+            {
+                if (diccionario.Contains(palabra))
+                {
+                    Console.WriteLine(diccionario[palabra]);
+                }
+                else
+                {
+                    Console.WriteLine("Palabra no encontrada");
+                }
+            }
+        } while (palabra != "");
+    }
+}
+
