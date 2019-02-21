d49562612f7af66c2c1b72336f90ce85c6ebe7a8
nachosv <nacho@iessanvicente.com>
2019-02-21 20:12:34 +0100

    Galaxian SDL 012: Loading screen

--- projects/galaxianSDL/inUse/GalaxianSDL/LoadingScreen.cs
+++ projects/galaxianSDL/inUse/GalaxianSDL/LoadingScreen.cs
@@ -2,17 +2,26 @@
 
 // Version + Date   Author + Changes
 // --------------   --------------------------------------
+// 012, 21-Feb-19   Enrique, Gonzalo: Login screen for 3 seconds
 // 009, 21-Feb-19   Nacho: Empty skeleton
 
 class LoadingScreen
 {
+    protected Image loginImage;
+
     public LoadingScreen()
     {
+        loginImage = new Image("data/galaxian_logo.png");
     }
 
     public void Run()
     {
-        // TO DO: Display Logo for 3 seconds and then finish
+        SdlHardware.ClearScreen();
+        SdlHardware.DrawHiddenImage(loginImage, 175, 300);
+        SdlHardware.ShowHiddenScreen();
+
+        SdlHardware.Pause(3000);
     }
 }
+
 
