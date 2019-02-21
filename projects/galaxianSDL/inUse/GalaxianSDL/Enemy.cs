0f18dcb1205571332fb7c2c73b42304749e00926
nachosv <nacho@iessanvicente.com>
2019-02-21 20:09:59 +0100

    Galaxian SDL 011: Enemy, first approach

--- projects/galaxianSDL/inUse/GalaxianSDL/Enemy.cs
+++ projects/galaxianSDL/inUse/GalaxianSDL/Enemy.cs
@@ -2,8 +2,29 @@
 
 // Version + Date   Author + Changes
 // --------------   --------------------------------------
+// 011, 21-Feb-19   Sergio, Diego: Constructor + Move
 // 009, 21-Feb-19   Nacho: Empty skeleton
+
 
 class Enemy : Sprite
 {
+
+    public Enemy(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        width = 33;
+        height = 24;
+        xSpeed = 3;
+        LoadImage("data/enemy1a.png");
+    }
+
+    public override void Move()
+    {
+        this.x += xSpeed;
+        if (x <= 50 || x >= 950)
+        {
+            xSpeed = -xSpeed;
+        }
+    }
 }
