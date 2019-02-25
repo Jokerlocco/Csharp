9a347b6a3531dac2d09dd5e486d2c8d5753cf756
Nacho Cabanes <nacho@nachocabanes.com>
2019-02-25 22:28:11 +0100

    Exercise 381: SortedList + Enumerator

---
+++ chapter08-dynamicMemory/381-SortedListEnumerator.cs
@@ -0,0 +1,27 @@
+﻿// Enumerator for a SortedList
+
+using System;
+using System.Collections;
+
+public class SortedListEnumerator
+{
+    public static void Main()
+    {
+        SortedList myDictionary = new SortedList();
+        myDictionary.Add("hoygan", "oigan");
+        myDictionary.Add("porfabor", "por favor");
+        myDictionary.Add("nesecito", "necesito");
+        myDictionary.Add("alluda", "ayuda");
+        myDictionary.Add("alludar", "ayudar");
+        myDictionary.Add("grasias", "gracias");
+        myDictionary.Add("ejersisio", "ejercicio");
+
+        IDictionaryEnumerator myEnumerator =
+            myDictionary.GetEnumerator();
+        while (myEnumerator.MoveNext())
+        {
+            Console.WriteLine(myEnumerator.Key + " -> " +
+                myEnumerator.Value);
+        }
+    }
+}
