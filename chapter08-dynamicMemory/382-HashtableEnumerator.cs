5d440817fe2dd8c750afa7c2d856028d4c9cb065
Nacho Cabanes <nacho@nachocabanes.com>
2019-02-25 22:35:53 +0100

    Exercise 382: Hashtable + Enumerator

---
+++ chapter08-dynamicMemory/382-HashtableEnumerator.cs
@@ -0,0 +1,27 @@
+﻿// Enumerator for a Hashtable
+
+using System;
+using System.Collections;
+
+public class HashtableEnumerator
+{
+    public static void Main()
+    {
+        Hashtable myDictionary = new Hashtable();
+        myDictionary.Add("hoygan", "oigan");
+        myDictionary.Add("porfabor", "por favor");
+        myDictionary.Add("nesecito", "necesito");
+        myDictionary.Add("alluda", "ayuda");
+        myDictionary.Add("alludar", "ayudar");
+        myDictionary.Add("grasias", "gracias");
+        myDictionary.Add("ejersisio", "ejercicio");
+
+        IDictionaryEnumerator myEnumerator =
+            myDictionary.GetEnumerator();
+        while (myEnumerator.MoveNext())
+        {
+            Console.WriteLine(myEnumerator.Key + " -> " +
+                myEnumerator.Value);
+        }
+    }
+}
