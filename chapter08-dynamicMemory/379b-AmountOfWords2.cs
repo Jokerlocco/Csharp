cb38656daf2fbb529ecbc3d1b059bcff188abf57
Nacho Cabanes <nacho@nachocabanes.com>
2019-02-25 22:24:43 +0100

    Exercise 379: Amount of distinct words

---
+++ chapter08-dynamicMemory/379b-AmountOfWords2.cs
@@ -0,0 +1,25 @@
+﻿/*
+Amount of (different) words in a sentence
+*/
+
+using System;
+using System.Collections;
+
+public class AmountOfWords
+{
+    public static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string text = Console.ReadLine();
+        SortedList list = new SortedList();
+
+        string[] parts = text.ToLower().Split();
+
+        foreach (string part in parts)
+        {
+            if (! list.Contains(part))
+                list.Add(part, "");
+        }
+        Console.WriteLine(list.Count);
+    }
+}
---
+++ chapter08-dynamicMemory/379c-AmountOfWords3.cs
@@ -0,0 +1,25 @@
+﻿/*
+Amount of (different) words in a sentence
+*/
+
+using System;
+using System.Collections.Generic;
+
+public class AmountOfWords
+{
+    public static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string text = Console.ReadLine();
+        Dictionary<string,bool> list = new Dictionary<string,bool>();
+
+        string[] parts = text.ToLower().Split();
+
+        foreach (string part in parts)
+        {
+            if (! list.ContainsKey(part))
+                list.Add(part, true);
+        }
+        Console.WriteLine(list.Count);
+    }
+}
