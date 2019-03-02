efbdb19b8a4ed58d5b8ff51c3869698ac3dc5aff
Nacho Cabanes <nacho@nachocabanes.com>
2019-03-02 14:38:42 +0100

    Exercise 393: ArraysAndPointers

---
+++ chapter08-dynamicMemory/393-ArraysAndPointers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class ArraysAndPointers
+{
+
+    public unsafe static void Main()
+    {
+        const int SIZE = 5;
+        int* data = stackalloc int[SIZE];
+
+        for (int i = 0; i < SIZE; i++)
+            data[i] = i * 10;
+
+        for (int i = 0; i < SIZE; i++)
+            Console.Write(data[i]+ " ");
+        Console.WriteLine();
+
+        int* start = data;
+        for (int i = 0; i < SIZE; i++)
+        {
+            Console.Write(*start + " ");
+            start++;
+        }
+        Console.WriteLine();
+    }
+}
