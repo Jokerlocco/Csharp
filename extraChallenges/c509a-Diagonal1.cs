491330b4c51ea493ad7981578b36d68df93ba26f
Nacho Cabanes <nacho@nachocabanes.com>
2019-03-11 22:42:05 +0100

    Challenge 05.09: Diagonal walking

---
+++ extraChallenges/c509a-Diagonal1.cs
@@ -0,0 +1,36 @@
+﻿// José Vicente Antón Castelló
+
+/*
+nput
+5
+RUURU
+output
+3
+ 
+input
+17
+UUURRRRRUUURURUUU
+output
+13
+*/
+
+using System;
+
+class DiagonalWalking
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string cadena = Console.ReadLine();
+
+        while (cadena.Contains("RU")
+           || cadena.Contains("UR"))
+        {
+            cadena = cadena.Replace("RU", "D");
+            cadena = cadena.Replace("UR", "D");
+        }
+
+        Console.WriteLine(cadena.Length);
+    }
+}
