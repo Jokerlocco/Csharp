76a0abb8b59eeb4f05b490705eda79889396f109
Nacho Cabanes <nacho@nachocabanes.com>
2019-03-11 22:21:19 +0100

    Exercise 395: StreamWriter 1

---
+++ chapter09-files/395-StreamWriter1.cs
@@ -0,0 +1,14 @@
+﻿// Gonzalo Arques
+using System;
+using System.IO;
+
+public class StreamWriter1
+{
+    public static void Main()
+    {
+        StreamWriter myFile = File.CreateText("test.txt");
+        myFile.WriteLine("Hi!");
+        myFile.WriteLine("Bye!");
+        myFile.Close();
+    }
+}
