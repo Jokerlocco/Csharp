5345778bf2e60c5c0e7b60ae08a8bbe5d01e2b6f
Nacho Cabanes <nacho@nachocabanes.com>
2019-03-14 22:35:12 +0100

    Exercise 405: File in another folder

---
+++ chapter09-files/405-OtherFolder.cs
@@ -0,0 +1,26 @@
+﻿// Manuel Lago
+
+using System;
+using System.IO;
+
+class OtherFolder
+{   
+    static void Main()
+    {
+        StreamReader file = 
+            new StreamReader(@"..\..\MyProject.csproj");
+
+        string line = file.ReadLine();
+        while(line != null)
+        {
+            if(line != null)
+            {
+                Console.WriteLine(line);
+            }
+
+            line = file.ReadLine();
+        }
+        file.Close();
+    }
+}
+
