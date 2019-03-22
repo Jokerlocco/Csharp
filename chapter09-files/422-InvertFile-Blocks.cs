5dfc7559b834d3493e6f755e9693f1193bddde5d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-03-22 17:44:56 +0100

    Exercise 422: Invert a file (blocks)

---
+++ chapter09-files/422-InvertFile-Blocks.cs
@@ -0,0 +1,39 @@
+//Ivan Lazcano Sindin
+using System;
+using System.IO;
+
+class FileCopyBlocks
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+            Console.WriteLine("Please specify source and destination");
+        else if(!File.Exists(args[0]))
+            Console.WriteLine("Origin does not exist");
+        else
+            try
+            {
+                FileStream readFile = File.OpenRead(args[0]);
+                int size = (int) readFile.Length;
+                byte[] data = new byte[size];
+                readFile.Read(data, 0, size);
+                readFile.Close();
+
+                if(File.Exists(args[1]))
+                    Console.WriteLine("File exists...");
+                else
+                {
+                    FileStream writeFile = File.Create(args[1]);
+                    Array.Reverse(data);
+                    writeFile.Write(data, 0, size);
+                    writeFile.Close();
+                }
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error on file!");
+            }
+
+    }
+}
