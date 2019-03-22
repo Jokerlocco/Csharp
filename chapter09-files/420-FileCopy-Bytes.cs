078b91510061848d2cefa1e25bcdd2502589f719
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-03-22 16:54:24 +0100

    Exercise 420: File copy (bytes)

---
+++ chapter09-files/420-FileCopy-Bytes.cs
@@ -0,0 +1,61 @@
+﻿//Kevin Marín Romero
+
+using System;
+using System.IO;
+
+public class FileCopyBytes
+{
+    static void Main(string[] args)
+    {
+        string sourceName, destName;
+        if (args.Length == 2)
+        {
+            sourceName = args[0];
+            destName = args[1];
+        }
+        else
+        {
+            Console.Write("Enter the name of source file: ");
+            sourceName = Console.ReadLine();
+            Console.Write("Enter the name of destination file: ");
+            destName = Console.ReadLine();
+        }
+
+        if(!File.Exists(sourceName))
+        {
+            Console.WriteLine("Source file Not Found");
+        }
+        else
+        {
+            if (File.Exists(destName))
+            {
+                Console.WriteLine("Destination file exists");
+            }
+            else
+            {
+                try
+                {
+                    FileStream fileIn = File.OpenRead(sourceName);
+                    FileStream fileOut = File.Create(destName);
+
+                    for (int i = 0; i < fileIn.Length; i++)
+                    {
+                        byte b = (byte)fileIn.ReadByte();
+                        fileOut.WriteByte(b);
+                    }
+
+                    fileOut.Close();
+                    fileIn.Close();
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("IO: " + e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
