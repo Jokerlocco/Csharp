9b46d0db5c7d652544749d33ccb2473c40a356cd
Nacho Cabanes <nacho@nachocabanes.com>
2019-04-12 19:38:07 +0200

    Exercise 460: RegEx1 - IsValidIdentifier + Tests

---
+++ chapter10-libraries/460-RegEx1Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Regex1
+{
+    public static void Main()
+    {
+        bool debugging = true;
+        if (debugging) TestIsValidIdentifier();
+        
+        Console.WriteLine("Enter the identifier: ");
+        string dato = Console.ReadLine();
+
+        Console.WriteLine(IsValidIdentifier(dato));
+    }
+
+    public static bool IsValidIdentifier(string s)
+    {
+        Regex r = new Regex(@"\A[a-zA-Z][A-Za-z0-9]*\z");
+        return r.IsMatch(s);
+    }
+    
+    public static void TestIsValidIdentifier()
+    {
+        Dictionary<string,bool> tests = new Dictionary<string,bool>();
+        
+        tests.Add("hola", true);
+        tests.Add("hola1", true);
+        tests.Add("Hola12", true);
+        tests.Add("H", true);
+        
+        tests.Add("1hola", false);
+        tests.Add("ho-la", false);
+        tests.Add("Hola ", false);
+        
+        foreach( KeyValuePair<string,bool> test in tests)
+        {
+            string data = test.Key;
+            bool expectedAnswer = test.Value;
+            bool realAnswer = IsValidIdentifier(data);
+            
+            if (realAnswer == expectedAnswer)
+                Console.Write("OK ");
+            else
+                Console.Write("Fail:" + data+" ");
+        }
+        
+    }
+}
