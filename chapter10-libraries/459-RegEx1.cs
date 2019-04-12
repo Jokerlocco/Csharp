12c70fc07de42145fbcee565a369b720103d1eb1
Nacho Cabanes <nacho@nachocabanes.com>
2019-04-12 19:36:19 +0200

    Exercise 459: RegEx1 - IsValidIdentifier

---
+++ chapter10-libraries/459-RegEx1.cs
@@ -0,0 +1,20 @@
+﻿// Gonzalo Arques
+using System;
+using System.Text.RegularExpressions;
+
+public class Regex1
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the identifier: ");
+        string dato = Console.ReadLine();
+
+        Console.WriteLine(IsValidIdentifier(dato));
+    }
+
+    public static bool IsValidIdentifier(string s)
+    {
+        Regex r = new Regex(@"\A[a-zA-Z][A-Za-z0-9]*\z");
+        return r.IsMatch(s);
+    }
+}
