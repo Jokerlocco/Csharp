8eda484fad2550e208960a1634e8ef801e454c73
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-05-09 11:30:26 +0200

    Exercise 471: Displaying data from SQLite

---
+++ chapter12-databases/471-SQLite-View.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SQLite;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SQLiteConnection conexion =
+            new SQLiteConnection
+            ("Data Source=series.sqlite;Version=3;New=False;Compress=True;");
+        conexion.Open();
+
+        string consulta = "select * from series";
+        SQLiteCommand cmd = new SQLiteCommand(consulta, conexion);
+        SQLiteDataReader datos = cmd.ExecuteReader();
+        while (datos.Read())
+        {
+            string titulo = Convert.ToString(datos[1]);
+            int anyo = Convert.ToInt32(datos[3]);
+            Console.WriteLine(titulo + " ("+anyo+")");
+        }
+        conexion.Close();
+    }
+}
