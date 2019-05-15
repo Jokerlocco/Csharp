3e3581907795d72060845e8a995ff9ccf281dbd6
Nacho Cabanes <nacho@nachocabanes.com>
2019-05-15 19:26:31 +0200

    Galaxian SDL 028: CreditsScreen - Moving credits

--- projects/galaxianSDL/inUse/GalaxianSDL/CreditsScreen.cs
+++ projects/galaxianSDL/inUse/GalaxianSDL/CreditsScreen.cs
@@ -2,6 +2,7 @@
 
 // Version + Date   Author + Changes
 // --------------   --------------------------------------
+// 028, 06-May-19   Nacho: Moving credits. Size changed from 18 to 24
 // 017, 21-Feb-19   Javier y Daniel: Names of the dev team
 // 009, 21-Feb-19   Nacho: Empty skeleton
 
@@ -9,7 +10,7 @@ class CreditsScreen
 {
     const short START_Y_POSITION = 225;
 
-    private Font font18;
+    private Font font24;
     private string[] names = { "Pablo", "Iván", "Cristi", "María", "Adrián", "Sergio", "Diego", "Miguel",
         "Kevin", "Jaime", "Araceli", "Ruth", "Saúl", "Jorge", "Enrique", "Gonzalo", "Miguel", "José",
         "Manuel", "Javier", "Daniel", "Nacho"};
@@ -23,9 +24,9 @@ class CreditsScreen
 
     public CreditsScreen()
     {
-        font18 = new Font("data/Joystix.ttf", 18);
+        font24 = new Font("data/Joystix.ttf", 24);
 
-        namesX = 375;
+        namesX = 450;
         namesY = START_Y_POSITION;
 
         red = 255;
@@ -35,52 +36,64 @@ class CreditsScreen
 
     public void Run()
     {
-        SdlHardware.ClearScreen();
-        SdlHardware.WriteHiddenText("(Partial) Remake by 1ºDAM",
-            340, 130,
-            255, 255, 255,
-            font18);
-        for (int i = 0; i < names.Length; i++)
-        {
-            if (i % 1 == 0)
-            {
-                red = 0;
-                green = 190;
-                blue = 255;
-            }
-
-            if (i % 2 == 1)
-            {
-                red = 0;
-                green = 0;
-                blue = 255;
-            }
-
-            if (i % 3 == 2)
-            {
-                red = 255;
-                green = 0;
-                blue = 255;
-            }
-
-            SdlHardware.WriteHiddenText(names[i], namesX, namesY, red, green, blue, font18);
-            namesY += 30;
-
-            if (i % 11 == 10)
-            {
-                namesY = START_Y_POSITION;
-                namesX += 200;
-            }
-        }
-        SdlHardware.WriteHiddenText("R to Return",
-            440, 620,
-            255, 0, 0,
-            font18);
-        SdlHardware.ShowHiddenScreen();
-
+        int displacement = 0;
+        int waveSize = 240;
+        float degrees = 0;
         do
         {
-            SdlHardware.Pause(100); // To avoid using 100% CPU
+            SdlHardware.ClearScreen();
+            SdlHardware.WriteHiddenText(
+                "(Partial) Remake by students of 1 DAM, "
+                    +"I.E.S. San Vicente, 2018/19",
+                50, 130,
+                255, 255, 255,
+                font24);
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (i % 1 == 0)
+                {
+                    red = 0;
+                    green = 190;
+                    blue = 255;
+                }
+
+                if (i % 2 == 1)
+                {
+                    red = 0;
+                    green = 0;
+                    blue = 255;
+                }
+
+                if (i % 3 == 2)
+                {
+                    red = 255;
+                    green = 0;
+                    blue = 255;
+                }
+
+                displacement = (int) (waveSize
+                    * System.Math.Sin(degrees * 3.14f / 360.0));
+                degrees += 0.1f;
+                SdlHardware.WriteHiddenText(
+                    names[i], 
+                    (short) (namesX+displacement), namesY, 
+                    red, green, blue, font24);
+                namesY += 30;
+
+                if (i % 11 == 10)
+                {
+                    namesY = START_Y_POSITION;
+                    namesX += 200;
+                }
+            }
+            SdlHardware.WriteHiddenText("R to Return",
+                440, 620,
+                255, 0, 0,
+                font24);
+            SdlHardware.ShowHiddenScreen();
+
+            SdlHardware.Pause(20); // 50 fps
+            namesX = 450;
         }
         while (!SdlHardware.KeyPressed(SdlHardware.KEY_R));
     }
